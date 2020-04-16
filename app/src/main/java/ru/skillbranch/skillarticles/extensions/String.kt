b8ff59e41b7b84202dc1b893049b26bcbf7f46eb
RareScrap <rarescrap@users.noreply.github.com>
2020-04-16 12:17:59 +0900

    feat(core): Задание 1

--- app/src/main/java/ru/skillbranch/skillarticles/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/extensions/String.kt
@@ -1,6 +1,14 @@
 package ru.skillbranch.skillarticles.extensions
 
-public fun String?.indexesOf(query: String?): List<Int> {
-    // TODO
-    return listOf<Int>() // TODO: Почему тут нельзя убрать return?
+public fun String?.indexesOf(substr: String, ignoreCase: Boolean = true): List<Int> {
+    var list = mutableListOf<Int>() // TODO: Чекнуть другие решения
+    if (substr.isNullOrBlank()) return list
+    var count = 0;
+    this?.split(substr, ignoreCase = ignoreCase)?.forEach {
+        count += it.length
+        list.add(count)
+        count += substr.length
+    }
+    list.remove(list.get(list.size-1))
+    return list // TODO: Почему тут нельзя убрать return?
 }
