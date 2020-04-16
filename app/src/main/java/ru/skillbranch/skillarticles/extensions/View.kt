6decdb396fc5c8bb32c4935b3b7ab2b1d7790663
RareScrap <rarescrap@users.noreply.github.com>
2020-04-16 12:52:01 +0900

    feat(core): Задание 2

--- app/src/main/java/ru/skillbranch/skillarticles/extensions/View.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/extensions/View.kt
@@ -1,7 +1,11 @@
 package ru.skillbranch.skillarticles.extensions
 
 import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.*
 
-public fun View.setMarginOptionally(bottom: Int) {
-
+public fun View.setMarginOptionally(left:Int = marginLeft, top : Int = marginTop, right : Int = marginRight, bottom : Int = marginBottom) {
+    updateLayoutParams<ViewGroup.MarginLayoutParams>{
+        setMargins(left, top, right, bottom)
+    }
 }
