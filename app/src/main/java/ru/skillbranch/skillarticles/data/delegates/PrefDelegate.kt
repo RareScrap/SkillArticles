4c7ae644a20cb51dcf79e53474e2325acf5b91bf
RareScrap <rarescrap@users.noreply.github.com>
2020-04-18 14:36:52 +0900

    feat(core): Попытка задания 7

---
+++ app/src/main/java/ru/skillbranch/skillarticles/data/delegates/PrefDelegate.kt
@@ -0,0 +1,30 @@
+package ru.skillbranch.skillarticles.data.delegates
+
+import ru.skillbranch.skillarticles.data.local.PrefManager
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+class PrefDelegate<T>(private val defaultValue: T) : ReadWriteProperty<PrefManager, T?> {
+
+    override fun getValue(thisRef: PrefManager, property: KProperty<*>): T? {
+        return when (defaultValue) {
+            is Int -> thisRef.preferences.getInt(property.name, defaultValue as Int) as T
+            is Boolean -> thisRef.preferences.getBoolean(property.name, defaultValue as Boolean) as T
+            is Float -> thisRef.preferences.getFloat(property.name, defaultValue as Float) as T
+            is String -> thisRef.preferences.getString(property.name, defaultValue as String) as T
+            is Long -> thisRef.preferences.getLong(property.name, defaultValue as Long) as T
+            else -> throw RuntimeException("Ебанный пиздец. Я попаду в ад за это")
+        }
+    }
+
+    override fun setValue(thisRef: PrefManager, property: KProperty<*>, value: T?) {
+        when (defaultValue) {
+            is Int -> thisRef.preferences.edit().putInt(property.name, defaultValue as Int).apply()
+            is Boolean -> thisRef.preferences.edit().putBoolean(property.name, defaultValue as Boolean).apply()
+            is Float -> thisRef.preferences.edit().putFloat(property.name, defaultValue as Float).apply()
+            is String -> thisRef.preferences.edit().putString(property.name, defaultValue as String).apply()
+            is Long -> thisRef.preferences.edit().putLong(property.name, defaultValue as Long).apply()
+            else -> throw RuntimeException("Ебанный пиздец. Я попаду в ад за это")
+        }
+    }
+}
