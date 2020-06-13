5face17d7c4cb2aa2df457fbbeb8b7573d3f9498
RareScrap <rarescrap@users.noreply.github.com>
2020-06-13 17:02:55 +0900

    feat(todo): Устранена TODO'ха

--- app/src/main/java/ru/skillbranch/skillarticles/ui/base/BaseActivity.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/base/BaseActivity.kt
@@ -8,7 +8,38 @@ import ru.skillbranch.skillarticles.viewmodels.base.IViewModelState
 import ru.skillbranch.skillarticles.viewmodels.base.Notify
 import ru.skillbranch.skillarticles.viewmodels.base.ViewModelDelegate
 
-// TODO: Что означают эти дженерики?
+/*
+Так уж устроено, что компилятор java не воспринимает BaseActivity<ArticleViewModel> как
+подтип BaseViewModel<IViewModelState>. Даже не смотря на то что ArticleViewModel имплементит
+IViewModelState.
+
+Java в таком случае требует вилдкарт BaseViewModel<? extends IViewModelState>, что в
+каком-то роде бессмысленно, потому что у переменной такого типа мы можем вызывать те же
+методы, что и ранее. Стало быть, более сложный тип не добавляет смысла. Но компилятор
+этого не понимает.
+
+В Kotlin существует способ объяснить вещь такого рода компилятору. Он называется "вариантность
+на уровне объявления": мы можем объявить типовой параметр T класса BaseActivity таким образом,
+чтобы удостовериться, что он только возвращается (производится) членами BaseActivity<T>, и
+никогда не потребляется. Чтобы сделать это, нам необходимо использовать модификатор out
+
+Теперь мы можем сделать так:
+val baseActivities: BaseActivity<BaseViewModel<IViewModelState>>
+    = BaseActivity<ArticleViewModel>()
+
+val baseActivity: BaseActivity<out BaseViewModel<out IViewModelState>>
+           = RootActivity() // Всё в порядке, т.к. T — out-параметры
+
+Общее правило таково: когда параметр T класса С объявлен как out, он может использоваться
+только в out-местах в членах C. Но зато C<Base> может быть родителем C<Derived>, и это будет безопасно.
+
+Модификатор out определяет вариантность, и так как он указывается на месте объявления типового
+параметра, речь идёт о вариативности на месте объявления. Эта концепция противопоставлена
+вариативности на месте использования из Java, где маски при использовании типа делают типы
+ковариантными.
+
+См доки котлина для подробной инфы: https://kotlinlang.ru/docs/reference/generics.html
+ */
 abstract class BaseActivity<T: BaseViewModel<out IViewModelState>> : AppCompatActivity() {
     protected abstract val binding:Binding
     protected abstract val viewModel : T
